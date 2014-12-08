639049d44156334c8fd7a5e1e07d044ce2e571e1
Ryan S. Northrup <northrup@yellowapple.us>
2014-12-08 01:02:18 -0800

    Fix metadata.rb
    Was missing 'name' and 'recipe' directives.

--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,11 @@
+name             "bash-completion"
 maintainer       "Phil Cohen"
 maintainer_email "github@phlippers.net"
 license          "MIT"
 description      "Installs bash-completion"
 version          "0.2.0"
+
+recipe "default", "Install bash-completion"
 
 supports "ubuntu"
 supports "debian"
